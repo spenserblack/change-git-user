3eb7329ce87481d3fa92f9f8495e941ec4f84b05
Spenser Black <spenserblack01@gmail.com>
2021-01-20 11:26:58 -0500

    Allow deletion of user config(s) (#23)
    Resolves #5

---
+++ src/delete.rs
@@ -0,0 +1,19 @@
+use super::Users;
+use anyhow::Result;
+use console::Term;
+use dialoguer::{theme::ColorfulTheme, MultiSelect};
+
+pub fn main(mut users: Users, term: Term, theme: ColorfulTheme) -> Result<()> {
+    let keys: Vec<_> = users.keys().map(String::from).collect();
+
+    let selections = MultiSelect::with_theme(&theme)
+        .with_prompt("Select git config(s) to delete:")
+        .items(&keys)
+        .interact_on(&term)?;
+
+    for key in selections.into_iter().map(|i| &keys[i]) {
+        users.remove(key);
+    }
+
+    super::write_users(&users)
+}
